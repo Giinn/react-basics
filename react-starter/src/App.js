baa6b21e76287f675f96900f015b2f892033e7f5
Gintaras <gintaras.jasaitis@macaw.nl>
2021-02-05 09:44:39 +0200

    Event handler test.

--- react-starter/src/App.js
+++ react-starter/src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
 function App() {
+
+	const clickHandler = (e) => {
+		console.log(e);
+	}
+
 	return (
 		<div>
 			<h1>Hello</h1>
+			<button onClick={clickHandler} >Click</button>
 		</div>
 	);
 }
